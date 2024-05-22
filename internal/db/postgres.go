faed7ea5238a5c4c435ddcfb10b71ab32807a53c
Todor Popovic <todorpopovic@Todors-MacBook-Pro.local>
2024-05-22 12:04:52 +0200

    Fixed getting value from .env file

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -3,9 +3,16 @@ package db
 import (
 	"context"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
 	"os"
 )
 
 func NewPostgres(ctx context.Context) (*pgxpool.Pool, error) {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		logrus.Error("Error loading .env file")
+	}
 	return pgxpool.New(ctx, os.Getenv("DB_URI"))
 }
